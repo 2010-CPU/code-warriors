cb5a7daa6dcf1f5cfb0442901f8542dabfd9d217
root <root@DESKTOP-VT7A85C.localdomain>
2021-03-23 19:48:48 -0700

    fixed user tables

--- db/init_db.js
+++ db/init_db.js
@@ -16,8 +16,8 @@ async function buildTables() {
         id SERIAL PRIMARY key,
         firstName VARCHAR(255) NOT NULL,
         lastName TEXT NOT NULL,
-        email VARCAHAR(320) NOT NULL,
-        image VARCHAR(255) UNIQUE,
+        email VARCHAR(320) NOT NULL,
+        imageURL TEXT DEFAULT 'someone@gmail.com',
         username VARCHAR(255) UNIQUE NOT NULL,
         password VARCHAR(255) UNIQUE NOT NULL,
         "isAdmin" BOOLEAN DEFAULT false NOT NULL
