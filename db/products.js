b0cb0f96592a04e1ec721a8a2a5e664051f242a9
root <root@DESKTOP-VT7A85C.localdomain>
2021-03-24 20:41:49 -0700

    DB products file

---
+++ db/products.js
@@ -0,0 +1,21 @@
+const client = require('./client')
+
+const getAllProducts = async () => {
+    try {
+      const { rows: products } = await client.query (`
+      SELECT *
+      FROM products,
+      `);
+  
+      return products;
+  
+    } catch (error) {
+  
+      throw error;
+    }
+  
+}
+
+module.exports = {
+    getAllProducts
+}
