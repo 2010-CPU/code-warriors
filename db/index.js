12d1be5dddeb52b227e355d9932a051210d132f6
Crystal Joyce <crystaljoyce@me.com>
2021-03-25 12:49:42 -0700

    added users database adapters and orders database adapters

--- db/index.js
+++ db/index.js
@@ -14,5 +14,6 @@ module.exports = {
   // db methods
   ...require('./product'),
   ...require('./products'),
-  ...require('./users')
+  ...require('./users'),
+  ...require('./orders'),
 }
--- db/users.js
+++ db/users.js
@@ -1,5 +1,33 @@
 const client = require('./index');
 
+const getAllUsers = async () => {
+    try {
+        const { rows: users } = await client.query(` 
+            SELECT * 
+            FROM users; 
+        `)
+        return users; 
+    } catch (error) {
+        throw error; 
+    }
+}
+
+const getUserById = async (id) => { 
+    try {
+        const { rows: [user]} = await client.require(`
+        SELECT * 
+        FROM users 
+        WHERE id = $1; 
+        `, [id]); 
+
+        return user; 
+    } catch (error) {
+        throw error; 
+    }
+}
+
+
 module.exports = {
-    
+    getAllUsers,
+    getUserById, 
 }
