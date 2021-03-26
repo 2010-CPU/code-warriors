b883a7c7ed1633a4c482d3fb79542f48c60151e1
samrunyan5 <samjorzak@gmail.com>
2021-03-25 21:29:57 -0500

    users API routes are ready to go

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,12 @@
 require('dotenv').config();
 
-const apiRouter = require('express').Router();
+const express = require('express');
+const apiRouter = express.Router();
+const server = express();
+
+const jwt = require('jsonwebtoken');
+const {getUserById} = require('../db');
+const {JWT_SECRET} = process.env;
 
 apiRouter.get("/", (req, res, next) => {
   res.send({
@@ -8,10 +14,45 @@ apiRouter.get("/", (req, res, next) => {
   });
 });
 
+apiRouter.use(async (req, res, next) => {
+  const prefix = 'Bearer ';
+  const auth = req.header('Authorization');
+
+  if (!auth) {
+    next();
+  } else if (auth.startsWith(prefix)) {
+    const token = auth.slice(prefix.length);
+
+    try {
+      const {id} = jwt.verify(token, JWT_SECRET);
+
+      if (id) {
+        req.user = await getUserById(id);
+        next();
+      }
+
+    } catch (error) {
+      next(error);
+    }
+  } else {
+    next({message: `Authorization token must start with ${prefix}`})
+  }
+})
+
 const productsRouter = require('./products'); 
 apiRouter.use('/products', productsRouter);
 
 const usersRouter = require('./users');
 apiRouter.use('/users', usersRouter);
+
+server.use((req, res, next) => {
+  res.status(404).send({message: 'Not Found'});
+});
+
+server.use((error, req, res, next) => {
+  if (res.StatusCode < 400) {
+    res.status(500).send({message: error.message});
+  }
+});
 
 module.exports = apiRouter;
