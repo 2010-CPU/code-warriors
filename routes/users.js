0776c1fda0234467d299b2b8d0624302ddd2784a
samrunyan5 <samjorzak@gmail.com>
2021-03-25 20:05:50 -0500

    Working on register route

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const usersRouter = express.Router();
 
+const {} = require('../db')
 
+usersRouter.post('/register', async (req, res, next) => {
+    const {username, password} = req.body;
+
+    try {
+
+    } catch (error) {
+        next(error);
+    }
+})
 
 module.exports = usersRouter;
