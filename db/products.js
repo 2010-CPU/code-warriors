aac2358b4775a62faed3ebd9c20741e47f604e68
root <root@DESKTOP-VT7A85C.localdomain>
2021-03-27 21:38:51 -0700

    finished with products

--- db/products.js
+++ db/products.js
@@ -1,21 +1,75 @@
-const {client} = require('./client')
+const {client} = require('./client');
 
 const getAllProducts = async () => {
-    try {
-      const { rows: products } = await client.query (`
-      SELECT *
-      FROM products;
-      `);
+  try {
+    const { rows: products } = await client.query (`
+    SELECT *
+    FROM products;
+    `);
+    return products;
+  }catch (error){
+    throw error;
+  }
+};
 
-      return products;
-  
-    } catch (error) {
-  
-      throw error;
-    }
-  
-}
+const getOrderProductById = async ({id}) => {
+  try{
+    const {rows: order_products} = await client.query(`
+    SELECT *
+    FROM products
+    WHERE id=${id};
+    `);
+    return order_products;
+  }catch (error) {
+    throw error;
+  }
+};
+
+const addProductToOrder = async ({ orderId, productId, price, quantity }) => {
+  try{
+    const { rows: [order_products] } = await client.query(`
+    INSERT INTO order_products("orderID", "activityId", price, quantity)
+    VALUES ($1, $2, $3, $4)
+    RETURN *;
+    `, [orderId, productId, price, quantity]);
+    return order_products;
+  } catch (error){
+    throw error;
+  }
+};
+
+const updateOrderProduct = async ({ id, price, quantity }) => {
+  try{
+    const { rows: [order_products] } = await client.query(`
+    UPDATE order_products
+    SET price = #2, quantity = #3
+    WHERE id= $1
+    RETURNING *
+    `, [id, price, quantity])
+
+    return order_products;
+  }catch (error){
+    throw error;
+  }
+};
+
+const destroyOrderProduct = async (id) => {
+  try {
+    const {rows: [deletedOrderProduct]} = await client.query(`
+    DELETE FROM order_products
+    WHERE id=${id}
+    RETURNING *;
+    `);
+    return deletedOrderProduct;
+  }catch(error){
+    throw error;
+  }
+};
 
 module.exports = {
-    getAllProducts
+    getAllProducts,
+    getOrderProductById,
+    addProductToOrder,
+    updateOrderProduct,
+    destroyOrderProduct
 }
