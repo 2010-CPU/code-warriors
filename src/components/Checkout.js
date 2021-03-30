e4227d019a100c3319eb165f2c448c48fa76bc8f
Crystal Joyce <crystaljoyce@me.com>
2021-03-29 21:29:29 -0700

    merging

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -4,14 +4,10 @@ import Account from './Account';
 import Cart from './Cart';
 
 const Checkout = ({ order, user, token }) => {
-    // const {firstName, lastName, email, username, address, city, state, zip, imageURL} = user;
-    // const { id, status, userId, datePlaced } = order;
 
     return (<div className='checkout'>
         
     <h3> welcome to checkout</h3>
-
-    {/* <Cart> </Cart> */}
 
     { token 
     ?   <Account> </Account> 
