dcbf8c6ecc3753aca9171b6d34a0f5bebed8531a
dolfo9 <dolfonvrrt@gmail.com>
2021-04-05 15:01:12 -0700

    rendered cart and added add to cart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,11 +1,60 @@
-import React, {useState, useEffect} from 'react';
-import {Link} from 'react-router-dom'
-import { Redirect } from 'react-router';
+import React, {useState, useEffect, Fragment} from 'react';
+import {Link} from 'react-router-dom';
+import axios from 'axios';
+import {SmallProduct} from './Product'
 
-const Cart = ({product}) => {
+
+const Cart = (props) => {
+    const {token, user} = props
+    const [cart, setCart] = useState({})
+    
+    const fetchCart = async () =>{
+        try {
+            const response = await axios.get('api/orders/cart',{
+                headers:{
+                    'Authorization': `Bearer ${token}`
+                }
+            })
+
+            const {data} = response
+
+            if(data){
+                setCart(data)
+                
+            }
+        } catch (error) {
+            
+        }
+    }
+
+    useEffect(() => {
+        if(token){
+            fetchCart()
+        }
+    }, [token])
+
+    if(!token){
+        return <div>you must be logged in to view this</div>
+    }
 
     return (<div className='bg-image img1'>
-        <div>This page has been left blank intentionally. Come back and check out your cart soon!</div>
+        <div className="cart">
+
+        <h2>My Cart</h2>
+        <p>{cart.datePlaced}</p>
+        <p>{cart.id}</p>
+        <p>{cart.status}</p>
+        <h3>Products</h3>
+        {
+            cart.products ? cart.products.map((product) => {
+                return <Fragment key={product.id}>
+                <h4>Quantity: {product.quantity}</h4>
+                <SmallProduct product={product}/>
+                
+                </Fragment>
+            }) : null
+        }
+        </div>
 
         
     
