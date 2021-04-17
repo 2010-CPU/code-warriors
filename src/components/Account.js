9509797fa7690de105c8d353d462aa90ad86bdf0
Crystal Joyce <crystaljoyce@me.com>
2021-04-17 01:15:34 -0700

    fixing css

--- src/components/Account.js
+++ src/components/Account.js
@@ -6,7 +6,7 @@ import 'react-tabs/style/react-tabs.css';
 
 // allow profile image choice later
 
-const Account = ({user, token, reviews, setReviews, setTitle, setContent,setStars, setUserId, setProductId, orders, setOrders }) => {
+const Account = ({user, token, reviews, setReviews, orders, setOrders, setProduct, getReviews }) => {
     const {firstName, lastName, email, username, address, city, state, zip, imageURL} = user;
 
     const userReviews = reviews.filter( review => { 
@@ -31,7 +31,7 @@ const Account = ({user, token, reviews, setReviews, setTitle, setContent,setStar
             console.error(error);
         }
     }
-    
+
     const getPastOrders = async () => {
         const response = await fetch(`/api/users/${user.id}/orders`, {
             method: 'GET',
@@ -46,65 +46,59 @@ const Account = ({user, token, reviews, setReviews, setTitle, setContent,setStar
 
     useEffect( ()=> {
         getPastOrders();
-        // getReviews();
+        getReviews();
     }, [])
 
     if (token && username) {
-        return (<><div>      
-            <Tabs>
-                <TabList className="tab-list"> 
-                    <Tab> Profile</Tab>
-                    <Tab> Orders </Tab>
-                    <Tab> Past Reviews </Tab>
-                    <Tab> Create Review</Tab>
-                </TabList>  
-                <TabPanel> 
-                    <div className='acct-container'> 
-                    {/* <h2>Account Information for {firstName}</h2> */}
+        return (<><div >    
+            <Tabs>        
+            <TabList className='tab-list'>
+                <Tab> Profile</Tab>
+                <Tab> Orders </Tab>
+                <Tab> Reviews </Tab>
+            </TabList>
+            <TabPanel> 
+                <div className='acct-container'> 
+                    <h3>Account Information</h3>
                     <img className='profile-image' src={imageURL} alt='userphotolink' />
                     <div className='profile'> 
-                    <div>Username: </div> <div> {username}</div>
-                    <div>Email: </div> <div> {email}</div>
-                    <div>Name:</div> <div> {firstName} {lastName}</div> 
-                    <div>Address: </div> <div> {address}<br/> {city}, {state} {zip}</div>
+                        <div>Username: </div> <div> {username}</div>
+                        <div>Email: </div> <div> {email}</div>
+                        <div>Name:</div> <div> {firstName} {lastName}</div> 
+                        <div>Address: </div> <div> {address}<br/> {city}, {state} {zip}</div>
                     </div>
-                    </div> 
-                </TabPanel>
+                </div> 
+            </TabPanel>
+            <TabPanel>
+            <div className='past-orders-container'>
+                <h3>Past Orders</h3>
+                {orders.length > 1 ? orders.map(order => {
+                    return order.status === 'completed' ?
+                        <PastOrders key={order.id} order={order} setProduct={setProduct} /> : ''
+                }) : 'You have no past orders!'}
+            </div>
+            </TabPanel>
+            <TabPanel>
+            <div className="acct-view-revs"> 
+            <h3> Reviews From Past Orders  </h3> <br/>
+            {userReviews.map((review) => { 
+                const {id, title, content, stars} = review;
 
-                <TabPanel>
-                    <div className='past-orders-container'>
-                        <h3>Past Orders</h3>
-                        {orders.length > 1 ? orders.map(order => {
-                            return order.status === 'completed' ?
-                                <PastOrders key={order.id} order={order} /> : ''
-                        }) : 'You have no past orders!'}
-                    </div>
-                </TabPanel>
-
-                <TabPanel>
-                    <div className="acct-view-revs"> 
-                    <h3 > Your reviews from your past orders:  </h3> <br/>
-                    {userReviews.map((review) => { 
-                        const {id, title, content, stars, userId, productId} = review;
-                        return <div key={id}> 
-                            <div className='rev-stars'> {stars > 4 ? <img src={'/images/5_stars.png'}/> : <img src={'/images/4_stars.png'}/>} </div>
-                            <div> Title: {title} </div> 
-                            <div>  Review:  {content} </div> <hr></hr>
-                        </div> 
-                    })}
-                    </div>
-                    </TabPanel>
-                    <TabPanel>
-                    <div className="new-rev">
-                    {/* <AddReview reviews={reviews} setReviews={setReviews} token={token} user={user} title={title} setTitle={setTitle} content={content} setContent={setContent} stars={stars} setStars={setStars} userId={userId} setUserId={setUserId} productId={productId} setProductId={setProductId} />               */}
-                    </div>
-                    </TabPanel>
+                return (<div key={id} className='review-detail'> 
+                    <br />
+                    <div className='rev-stars'> {stars > 4 ? <img src={'/images/5_stars.png'}/> : <img src={'/images/4_stars.png'}/>} </div>
+                    <div>Title: {title} </div> 
+                    <div>Review:  {content} </div>
+                    <button className='btn' onClick={() => handleDelete(review.id)}>Delete Review</button>
+                </div>)
+            })}
+            </div>
+            </TabPanel>
             </Tabs> 
-                    </div>
-                    </>)
+            </div> 
+            </>)
     } else {
         return <Redirect to='/' />
     }
 }
-
 export default Account;
